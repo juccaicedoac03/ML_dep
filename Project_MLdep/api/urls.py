73b2bca703dc2e85554a13891c41676524785540
Julian Caicedo <juccaicedoac@unal.edu.co>
2022-06-26 16:59:15 -0500

    update views

--- Project_MLdep/api/urls.py
+++ Project_MLdep/api/urls.py
@@ -3,4 +3,5 @@ from .views import OrderView
 
 urlpatterns = [
     path('Orders/', OrderView.as_view(),name='orders_list'),
+    path('Orders/<int:order_id>/', OrderView.as_view(),name='orders_process'),
 ]
--- Project_MLdep/api/views.py
+++ Project_MLdep/api/views.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django import dispatch
 from django.http import JsonResponse
 from django.shortcuts import render
@@ -8,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status, generics
 from .models import order
+import json
 
 class OrderView(View):
     
@@ -15,19 +17,55 @@ class OrderView(View):
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request): 
-        orders = list(order.objects.values())
+    def get(self, request, order_id=0):
+        if order_id > 0:
+            orders = list(order.objects.filter(order_id=order_id).values())
+            if len(orders)>0:
+                data = {'message': 'Success', 'orders': orders[0]}
+            else:
+                data = {'message': 'No orders found'}
+        else:    
+            orders = list(order.objects.values())
+            if len(orders)>0:
+                data = {'message': 'Success', 'orders': orders}
+            else:
+                data = {'message': 'No orders found'}
+        return JsonResponse(data)
+
+    def post(self, request):
+        #print(request.body)
+        jd = json.loads(request.body)
+        print(jd)
+        
+        order.objects.create(order_id=jd['order_id'],store_id=jd['store_id'],
+        to_user_distance=jd['to_user_distance'],to_user_elevation=jd['to_user_elevation'],
+        total_earning=jd['total_earning'],created_at=jd['created_at'],taken=jd['taken'])
+        data = {'message': 'Success'}
+        return JsonResponse(data)
+
+    def put(self, request, order_id):
+        jd = json.loads(request.body)
+        orders = list(order.objects.filter(order_id=order_id).values())
         if len(orders)>0:
-            data = {'message': 'Success', 'orders': orders}
+            up_order = order.objects.get(order_id=order_id)
+            up_order.order_id = jd['order_id']
+            up_order.store_id = jd['store_id']
+            up_order.to_user_distance = jd['to_user_distance']
+            up_order.to_user_elevation = jd['to_user_elevation']
+            up_order.total_earning = jd['total_earning']
+            up_order.created_at = jd['created_at']
+            up_order.taken = jd['taken']
+            up_order.save()
+            data = {'message': 'Success'}
         else:
             data = {'message': 'No orders found'}
         return JsonResponse(data)
 
-    def post(self, request):
-        pass
-
-    def put(self, request):
-        pass
-
-    def delete(self, request):
-        pass
+    def delete(self, request, order_id):
+        orders = list(order.objects.filter(order_id=order_id).values())
+        if len(orders)>0:
+            order.objects.get(order_id=order_id).delete()
+            data = {'message': 'Success'}
+        else:
+            data = {'message': 'No orders found'}
+        return JsonResponse(data)
