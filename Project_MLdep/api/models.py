555b1b130005b8b4dd45c7dd3ff8661b1d8a23bc
Julian Caicedo <juccaicedoac@unal.edu.co>
2022-06-26 00:55:48 -0500

    initial migrate

--- Project_MLdep/api/models.py
+++ Project_MLdep/api/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class orders(models.Model):
-    order_id = models.BigIntegerField(primary_key=True,max_length=10)
-    store_id = models.BigIntegerField(max_length=10)
+    order_id = models.BigIntegerField(primary_key=True)
+    store_id = models.BigIntegerField()
     to_user_distance = models.FloatField(max_length=200)
     to_user_elevation = models.FloatField(max_length=200)
-    total_earning = models.BigIntegerField(max_length=10)
+    total_earning = models.BigIntegerField()
     created_at = models.TextField(max_length=25)
     taken = models.BooleanField()
