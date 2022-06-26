77ffd28303ce6de0d41f58000e12c512d3528b26
Julian Caicedo <juccaicedoac@unal.edu.co>
2022-06-26 00:30:56 -0500

    create dataset

--- Project_MLdep/api/models.py
+++ Project_MLdep/api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class orders(models.Model):
     order_id = models.BigIntegerField(primary_key=True,max_length=10)
     store_id = models.BigIntegerField(max_length=10)
-    to_user_distance = models.FloatField(max_digits=10,decimal_places=10)
-    to_user_elevation = models.FloatField(max_digits=10,decimal_places=10)
+    to_user_distance = models.FloatField(max_length=200)
+    to_user_elevation = models.FloatField(max_length=200)
     total_earning = models.BigIntegerField(max_length=10)
     created_at = models.TextField(max_length=25)
     taken = models.BooleanField()
