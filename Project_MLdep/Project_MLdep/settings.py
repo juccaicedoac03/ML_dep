9f7e7cada43fb5c7b95559f704bff32324b964ac
Julian Caicedo <juccaicedoac@unal.edu.co>
2022-06-26 01:11:01 -0500

    create dataset

--- Project_MLdep/Project_MLdep/settings.py
+++ Project_MLdep/Project_MLdep/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'PORT': '3306',
         'USER': 'root',
         'PASSWORD': 'AdminSQL',
-        'NAME': 'DB_MLdep',
+        'NAME': 'MLdep',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
