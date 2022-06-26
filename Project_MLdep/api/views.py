67532f9bd8a3de06ca879058173a8474873a1804
Julian Caicedo <juccaicedoac@unal.edu.co>
2022-06-26 16:21:01 -0500

    add csrf exempt

--- Project_MLdep/api/views.py
+++ Project_MLdep/api/views.py
@@ -1,14 +1,21 @@
+from django import dispatch
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status, generics
 from .models import order
 
 class OrderView(View):
+    
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request):
+    def get(self, request): 
         orders = list(order.objects.values())
         if len(orders)>0:
             data = {'message': 'Success', 'orders': orders}
