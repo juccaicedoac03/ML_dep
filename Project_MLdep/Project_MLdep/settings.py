504c223691d885f10b577eddd9ef410deb817a12
Julian Caicedo <juccaicedoac@unal.edu.co>
2022-06-27 10:37:01 -0500

    update settings for deployment

--- Project_MLdep/Project_MLdep/settings.py
+++ Project_MLdep/Project_MLdep/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1blz7$youg7l!rvy7x8bg458kjty=b=s@$n%$icctue3=c$@r@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
