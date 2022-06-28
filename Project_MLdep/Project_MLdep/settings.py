aeb65ef936e6ae7d6fc9d2c08e2baf91efd1e2e6
Julian Caicedo <juccaicedoac@unal.edu.co>
2022-06-28 02:37:50 -0500

    update

--- Project_MLdep/Project_MLdep/settings.py
+++ Project_MLdep/Project_MLdep/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'Project_MLdep.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
